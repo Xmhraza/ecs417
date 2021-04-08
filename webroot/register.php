0bafb34b369f034b070bde1974e81d784a176166
Xmhraza <xmhraza123@gmail.com>
2021-04-08 14:25:34 +0100

    changed

---
+++ webroot/register.php
@@ -0,0 +1,30 @@
+<?php
+
+  session_start();
+
+    include("database.php");
+
+    $Fname = $_POST['Fname'];
+    $Lname = $_POST['Lname'];
+    $email = $_POST['email'];
+    $pwd = $_POST['password'];
+
+    mysqli_select_db($conn, 'USERS');
+
+    $s = "SELECT * from USERS where email = '$email'";
+
+    $result = mysqli_query($conn, $s);
+    $num = mysqli_num_rows($result);
+
+    if ($num > 0) {
+        echo "Email already taken";
+    } else {
+        $sql = "INSERT INTO USERS (firstName, lastName, email, password) 
+        VALUES ('$Fname', '$Lname', '$email', '$pwd')";     
+        
+        if ($conn->query($sql) === TRUE) {
+            echo "Registration Complete";
+        } else {            
+              echo "Error: " . $sql . "<br>" . $conn->error;         
+         }                 
+    }
