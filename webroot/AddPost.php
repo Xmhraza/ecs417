f2d5f0403e0e402041be7d5f6d11233103b56436
Xmhraza <xmhraza123@gmail.com>
2021-04-09 13:59:51 +0100

    changed

---
+++ webroot/AddPost.php
@@ -0,0 +1,17 @@
+<?php
+
+  session_start();
+
+    include("database.php");
+
+    $title = $_POST['title'];
+    $Blog = $_POST['Blog'];
+    $date = date('Y-m-d');
+    $time = ' ' + date('H:i:s');
+
+
+        $sql = "INSERT INTO Blog (Title, Blog, Date, Time) 
+        VALUES ('$title', '$Blog', '$date', '$time')";
+        
+        header('location:AddPost.html');  
+                   
---
+++ webroot/ViewBlog1.php
@@ -0,0 +1,42 @@
+<?php
+
+session_start();
+
+
+include("login.php");
+include("database.php");
+
+$s = "SELECT Date, Time from Blog" ;
+// run query
+$query = mysqli_query($conn ,$s);
+
+// set array
+$array = array();
+
+// look through query
+while ($row = mysqli_fetch_assoc($query)) {
+
+  array_push($array, $row);
+
+}
+
+print_r($array);
+
+function swapValues2( $array, $dex, $dex2 ) {
+    list($array[$dex],$array[$dex2]) = array($array[$dex2], $array[$dex]);
+    return $array;
+}
+ 
+
+ 
+function bubbleSort( $array)
+{
+    for( $out=0, $size = count($array); $out < $size - 1 ; $out++ )  {
+        for( $in = $out + 1; $in < $size; $in++ ) {
+              if (strtotime($array[ $out ]) > strtotime($array[ $in ])) {
+                $array = swapValues2($array, $out, $in);
+             }
+        }
+    }
+    return $array;  
+}
